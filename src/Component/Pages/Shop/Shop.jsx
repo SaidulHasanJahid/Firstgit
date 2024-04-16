62a6cb46c8bccb1211386bcbce0d9c3daf848f30
SaidulHasanJahid <jahid2002@gmail.com>
2024-04-17 00:36:47 +0600

    wi

--- src/Component/Pages/Shop/Shop.jsx
+++ src/Component/Pages/Shop/Shop.jsx
@@ -3,6 +3,7 @@ const Shop = () => {
     return (
         <div>
             <h1>This Is Shop</h1>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus error ullam enim excepturi totam consequuntur?</p>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate sunt, impedit architecto perspiciatis dolorem fugiat repellat rem id expedita, dolores dolorum officiis nisi aut, esse illo ad voluptatum! Esse, repellendus!</p>
         </div>
     );
